4dd708b72d4c69dbcc48bf2659cb32ea2c5e7776
Robert Quainoo <softdeveloperrob@gmail.com>
2024-06-02 20:04:50 +0000

    Created model and migration for student

---
+++ database/migrations/2024_06_02_195123_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_id')->unique();
+            $table->string('other_names');
+            $table->string('last_name');
+            $table->date('date_of_birth');
+            $table->integer('gender_id');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->date('enrollment_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
