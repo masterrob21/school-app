d0d67e3e62cb91e153557b3a80c0e5bc37e401e8
Robert Quainoo <softdeveloperrob@gmail.com>
2024-06-20 14:21:04 +0000

    Created model, migration for staff

---
+++ database/migrations/2024_06_20_093047_create_staff_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id();
+            $table->string('staff_id')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth');
+            $table->foreignId('gender_id');
+            $table->string('address');
+            $table->string('email');
+            $table->date('hire_date');
+            $table->foreignId('department_id');
+            $table->foreignId('branch_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('staff');
+    }
+};
