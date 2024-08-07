d2f0997643cad999dd91f4b51016915e4d19ac0f
Robert Quainoo <softdeveloperrob@gmail.com>
2024-08-07 16:30:34 +0000

    Capitalize the first letter of the model

--- app/Models/occupation.php
+++ app/Models/occupation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class occupation extends Model
+class Occupation extends Model
 {
     use HasFactory;
 }
