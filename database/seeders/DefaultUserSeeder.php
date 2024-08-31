6a8bd4db601933e2d6d0ce5e637984cf92ddbb5a
Robert Quainoo <softdeveloperrob@gmail.com>
2024-08-31 10:38:47 +0000

    Created seeder for default user account

---
+++ database/seeders/DefaultUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DefaultUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::insert([
+            'name' => 'Administrator',
+            'email' => 'admin@noreply.com',
+            'password' => bcrypt('admin1234'),
+            'is_system' => bcrypt('admin1234'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
