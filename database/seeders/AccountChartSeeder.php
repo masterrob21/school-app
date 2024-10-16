a03b3aa2af35fbcb545210dc39fd00b10dfb7f54
Robert Quainoo <softdeveloperrob@gmail.com>
2024-10-16 16:25:41 +0000

    Created model, migration, seeder for accountchart

---
+++ database/seeders/AccountChartSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AccountChartSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('account_charts')->insert([
+            ['account_head' => 'Property, Plant and Equipment', 'account_type_id' => '1', 'sort_order' => '10'],
+            ['account_head' => 'Cash and Cash Equivalent', 'account_type_id' => '1', 'sort_order' => '20'],
+            ['account_head' => 'Receivables', 'account_type_id' => '1', 'sort_order' => '30'],
+        ]);
+    }
+}
