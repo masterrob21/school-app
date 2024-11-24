93406850cd9027aa599c43e40fd9f7e388b3adef
Robert Quainoo <softdeveloperrob@gmail.com>
2024-11-24 11:53:29 +0000

    Created a model, seeder and migration for program_type

---
+++ database/seeders/ProgramTypeSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProgramTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('program_types')->insert([
+            ['program_type' => 'invoicing', 'created_at' => now(), 'updated_at' => now()],
+            ['program_type' => 'bad debt', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+}
